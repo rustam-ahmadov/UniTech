a37550c4b1dffc70a8f04710704c9ee619481d35
RustamAhmadov <ahmadov.rustam114@gmail.com>
2023-08-29 17:47:24 +0400

    Initial commit

---
+++ src/test/java/com/example/unitech/UniTechApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.unitech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniTechApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
